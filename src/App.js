f85f5c6d2c3eeba33b0e88c94257561815e0c85a
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2020-09-24 23:32:54 -0600

    Add skeew scrolling functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.scss";
+
+import useWindowSize from './hooks/useWindowSize';
 
 import images from "./images/images";
 
 function App() {
+  const size = useWindowSize();
+
+  const app = useRef();
+  const scrollContainer = useRef();
+
+  const skewConfigs = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0
+  }
+
+  useEffect(() => {
+    document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
+  }, [size.height]);
+
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, []);
+
+  const skewScrolling = () => {
+    skewConfigs.current = window.scrollY;
+    skewConfigs.previous += (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
+  
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew}deg)`
+
+    requestAnimationFrame(() => skewScrolling());
+  }
+
   return (
-    <div className='App'>
-      <div className='scroll'>
+    <div ref={app} className='App'>
+      <div ref={scrollContainer} className='scroll'>
         {images.map((image, index) => (
           <>
             <div key={index} className='img-container'>
